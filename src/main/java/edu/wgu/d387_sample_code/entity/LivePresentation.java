ee128f0480110f42fcee7c9bb877901146ef678b
Bobby Newsom <bobby.newsom@gmail.com>
2024-05-25 16:56:24 -0500

    Entity creation for LivePresentation

---
+++ src/main/java/edu/wgu/d387_sample_code/entity/LivePresentation.java
@@ -0,0 +1,88 @@
+package edu.wgu.d387_sample_code.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "live_presentations")
+public class LivePresentation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 255)
+    private String title;
+
+    @Column(name = "description", length = 500)
+    private String description;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "presenter", length = 100)
+    private String presenter;
+
+    @Column(name = "is_live")
+    private boolean isLive;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getPresenter() {
+        return presenter;
+    }
+
+    public void setPresenter(String presenter) {
+        this.presenter = presenter;
+    }
+
+    public boolean isLive() {
+        return isLive;
+    }
+
+    public void setLive(boolean live) {
+        isLive = live;
+    }
+
+    @Override
+    public String toString() {
+        return "LivePresentation{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", dateTime=" + dateTime +
+                ", presenter='" + presenter + '\'' +
+                ", isLive=" + isLive +
+                '}';
+    }
+}
